02e7c2e16c9bdc8adbc40ed594117315bc361610
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-01-13 19:22:48 +0100

    config title and change favicon

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+
+// Put this into /vue.config.js
 module.exports = {
+	chainWebpack: config => {
+	  config
+		.plugin('html')
+		.tap(args => {
+		args[0].title = 'MaxCoach';	// Replace your title here
+		return args;
+	  });
+	},
+
 	publicPath: './'
 };
